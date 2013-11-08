79258c7f20d9bb691a3c3829c156ce5ff5356ccc
Egor <12_123@mail.ru>
2013-11-08 16:56:23 +0400

    basic login logics

--- PlanViewer/Account/Login.aspx.cs
+++ PlanViewer/Account/Login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using PlanViewer.Models;
 
 namespace PlanViewer
 {
@@ -19,6 +22,76 @@ namespace PlanViewer
             {
                 RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
             }*/
+        }
+
+        protected void Customer_Authenticate(object sender, AuthenticateEventArgs e)
+        {            
+            string email = Customer.UserName;
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrEmpty(email))
+            {
+                Alert.Show("Некорректное имя пользователя!");
+                return;
+            }
+            string pass = Customer.Password;
+            if (string.IsNullOrWhiteSpace(pass) || string.IsNullOrEmpty(pass))
+            {
+                Alert.Show("Введите пароль!");
+                return;
+            }
+            
+            string connectionStr = WebConfigurationManager.ConnectionStrings["TeamProjectDBConnectionString1"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionStr);
+            con.Open();
+            string sqlUserName = "SELECT email,password FROM Customer WHERE email ='" + email + "' AND password ='" + pass + "'";
+            SqlCommand cmd = new SqlCommand(sqlUserName, con);
+            string CurrentName;
+            CurrentName = (string)cmd.ExecuteScalar();
+            if (CurrentName != null)
+            {
+                Session["UserAuthentication"] = email;
+                Session.Timeout = 1;
+                Response.Redirect("../ViewPlan.aspx");
+            }
+            else
+            {
+                Session["UserAuthentication"] = "";
+            }
+            con.Close();
+        }
+
+        protected void Contractor_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string email = Contractor.UserName;
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrEmpty(email))
+            {
+                Alert.Show("Некорректное имя пользователя!");
+                return;
+            }
+            string pass = Contractor.Password;
+            if (string.IsNullOrWhiteSpace(pass) || string.IsNullOrEmpty(pass))
+            {
+                Alert.Show("Введите пароль!");
+                return;
+            }
+
+            string connectionStr = WebConfigurationManager.ConnectionStrings["TeamProjectDBConnectionString1"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionStr);
+            con.Open();
+            string sqlUserName = "SELECT email,password FROM Contractor WHERE email ='" + email + "' AND password ='" + pass + "'";
+            SqlCommand cmd = new SqlCommand(sqlUserName, con);
+            string CurrentName;
+            CurrentName = (string)cmd.ExecuteScalar();
+            if (CurrentName != null)
+            {
+                Session["UserAuthentication"] = email;
+                Session.Timeout = 1;
+                Response.Redirect("../CreatePlan.aspx");
+            }
+            else
+            {
+                Session["UserAuthentication"] = "";
+            }
+            con.Close();
         }
     }
 }
